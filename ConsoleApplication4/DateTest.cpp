09df7b477c0feba6636de9b95ab017b57c3eab15
Esa P <Esa P>
2015-11-28 23:29:41 +0200

    leap years tested

--- ConsoleApplication4/DateTest.cpp
+++ ConsoleApplication4/DateTest.cpp
@@ -1,10 +1,24 @@
 #include "stdafx.h"
 
-TEST(NdTest, leap400) {
+TEST(NextDayTest, leap_years) {
+
 	Date date = Date(28, 2, 400);
 	date.next_day();
 	EXPECT_EQ(29, date.getDay());
+	EXPECT_EQ(2, date.getMonth());
+
+	date = Date(28, 2, 404);
+	date.next_day();
+	EXPECT_EQ(29, date.getDay());
+	EXPECT_EQ(2, date.getMonth());
+
+	date = Date(28, 2, 500);
+	date.next_day();
+	EXPECT_EQ(1, date.getDay());
+	EXPECT_EQ(3, date.getMonth());
+
 }
+
 
 int main(int ac, char* av[]) {
 	testing::InitGoogleTest(&ac, av);
